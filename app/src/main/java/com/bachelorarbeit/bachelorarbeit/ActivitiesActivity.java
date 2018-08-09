6c43cd5c381d029a815c35672cec6f515232f3c3
Sabine Roth <sabi.roth@t-online.de>
2018-08-09 19:53:39 +0200

    Activities Orte + Manuelle Eingabe

--- app/src/main/java/com/bachelorarbeit/bachelorarbeit/ActivitiesActivity.java
+++ app/src/main/java/com/bachelorarbeit/bachelorarbeit/ActivitiesActivity.java
@@ -1,5 +1,6 @@
 package com.bachelorarbeit.bachelorarbeit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -7,7 +8,9 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckedTextView;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -20,6 +23,7 @@ public class ActivitiesActivity extends AppCompatActivity {
     public ListView socialActivitiesListView;
     public ListView sportActivititesListView;
     public ListView obligationsActivitiesListView;
+    public EditText editTextField;
     public String[] socialActivitiesArrayString = {"Mit Freunden/Bekannten getroffen", "Mit Freunden/Bekannten telefoniert"};
     public String[] sportActivitiesArrayString = {"Spazieren gehen", "Schwimmen"};
     public String[] obligationsActivitiesArrayString = {"Zur Arbeit gegangen", "Einkaufen gegangen"};
@@ -32,6 +36,8 @@ public class ActivitiesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_activities);
 
         Button nextButton = (Button)findViewById(R.id.button_activities_next);
+        Button saveButton = (Button)findViewById(R.id.saveButton_activities);
+        editTextField = (EditText)findViewById(R.id.editText_activities);
         socialActivitiesListView = (ListView)findViewById(R.id.listView_activities1);
         sportActivititesListView = (ListView)findViewById(R.id.listView_activities2);
         obligationsActivitiesListView = (ListView)findViewById(R.id.listView_activities3);
@@ -75,6 +81,13 @@ public class ActivitiesActivity extends AppCompatActivity {
                 nextButtonClicked();
             }
         });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveButtonClicked();
+            }
+        });
     }
 
     private void entryClicked(String clickedEntry, View viewListEntry){
@@ -91,5 +104,16 @@ public class ActivitiesActivity extends AppCompatActivity {
 
     private void nextButtonClicked(){
 
+        Intent i = new Intent(this, PlacesActivity.class);
+        i.putExtra("selectedActivities", allSelectedEntries);
+        startActivity(i);
+    }
+
+    private void saveButtonClicked(){
+        allSelectedEntries.add(editTextField.getText().toString());
+        CharSequence text = "Gespeichert";
+        Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG);
+        toast.show();
+        editTextField.setText("");
     }
 }
--- app/src/main/java/com/bachelorarbeit/bachelorarbeit/PlacesActivity.java
+++ app/src/main/java/com/bachelorarbeit/bachelorarbeit/PlacesActivity.java
@@ -1,16 +1,75 @@
 package com.bachelorarbeit.bachelorarbeit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 
 
 public class PlacesActivity extends AppCompatActivity {
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_places);
+        Button endButton = (Button)findViewById(R.id.button_places_end);
+        endButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                endButtonClicked();
+            }
+        });;
+        LinearLayout layout = (LinearLayout)findViewById(R.id.layout_listentry_places);
+        Intent intent = getIntent();
+        ArrayList<String> selectedActivites = intent.getStringArrayListExtra("selectedActivities");
+        LinearLayout.LayoutParams paramsForTextView = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+         );
+        paramsForTextView.setMargins(25,40,25,15);
 
+        LinearLayout.LayoutParams paramsforEditView = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        paramsforEditView.setMargins(25, 0, 25, 35);
+
+        for(int i = 0; i<selectedActivites.size(); i++){
+            TextView entry = new TextView(getApplicationContext());
+            entry.setLayoutParams(paramsForTextView);
+            buildTextView(entry, selectedActivites.get(i));
+            entry.setText(selectedActivites.get(i));
+            layout.addView(entry);
+            EditText editText = new EditText(getApplicationContext());
+            editText.setLayoutParams(paramsforEditView);
+            buildEditText(editText);
+            layout.addView(editText);
+
+        }
+    }
+
+    private void buildTextView(TextView entry, String textContent){
+        entry.setText(textContent);
+        entry.setTextColor(getResources().getColor(android.R.color.black));
+        entry.setTextSize(23);
+    }
+
+    private void buildEditText(EditText editText){
+        editText.setHint("Ort eingeben");
+        editText.setInputType(InputType.TYPE_CLASS_PHONE);
+    }
+
+    private void endButtonClicked(){
+        Intent i = new Intent(this, HomeActivity.class);
+        startActivity(i);
     }
 }
