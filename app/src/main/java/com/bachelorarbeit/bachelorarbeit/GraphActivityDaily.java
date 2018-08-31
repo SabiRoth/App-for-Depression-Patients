432818fc01f8c0083c48eff34bfb28970fdc9da4
Sabine Roth <sabi.roth@t-online.de>
2018-08-31 16:58:01 +0200

    Update Graph

--- app/src/main/java/com/bachelorarbeit/bachelorarbeit/GraphActivityDaily.java
+++ app/src/main/java/com/bachelorarbeit/bachelorarbeit/GraphActivityDaily.java
@@ -5,137 +5,246 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.LegendRenderer;
-import com.jjoe64.graphview.helper.StaticLabelsFormatter;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
+import com.softmoore.android.graphlib.*;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 
+public class GraphActivityDaily extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
-public class GraphActivityDaily extends AppCompatActivity {
 
-    Spinner spinnerView, spinnerGraph;
     Button datePickerButton;
-    DateTimePicker dateTimePicker;
-    String pickedDate;
-    GraphView graph;
     String[] spinnerList;
-    String[] spinnerListGraph;
+    Spinner spinnerView, spinnerGraph;
+    String pickedDate;
+    DateTimePicker dateTimePicker;
+    ArrayList<String> symptomListInDb;
+    int check = 0;
+    dataSource dataSource;
+    String[] mainSymptoms;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_graph);
-        spinnerList = getResources().getStringArray(R.array.spinner_view_daily);
-        graph = (GraphView) findViewById(R.id.graph);
+        setContentView(R.layout.activity_graph_daily);
         datePickerButton = (Button) findViewById(R.id.button_date_picker_calendar);
+        spinnerList = getResources().getStringArray(R.array.spinner_view_daily);
         spinnerView = (Spinner) findViewById(R.id.spinner);
         spinnerGraph = (Spinner) findViewById(R.id.spinner_graph);
         dateTimePicker = DateTimePicker.getInstance();
+        symptomListInDb = new ArrayList<>();
+        dataSource = new dataSource(this);
+        dataSource.open();
+        mainSymptoms = (getResources().getString(R.string.key_score) + "," + dataSource.getSettingViaName(getResources().getString(R.string.key_mainSymptoms))).split(",");
         pickedDate = getIntent().getStringExtra(getResources().getString(R.string.key_date));
         if(pickedDate == null){
             pickedDate = dateTimePicker.getCurrentDate();
         }
         datePickerButton.setText(pickedDate);
-        initializeSpinner();
         initializeClickListenerForDatePickerButton();
-        initializeGraphView();
+        createGraph();
+        initializeSpinner();
     }
 
 
+    private void createGraph(){
+        String contentIntent = getIntent().getStringExtra(getResources().getString(R.string.key_spinner_graph));
+        Label[] xLabels = {new Label(1, getResources().getString(R.string.morning)), new Label(2, getResources().getString(R.string.midday)), new Label(3, getResources().getString(R.string.evening))};
 
-    private void initializeGraphView(){
-        dataSource dataSource = new dataSource(this);
-        dataSource.open();
-        ArrayList<Entry> entries  = dataSource.getAllEntries(pickedDate);
-        //ArrayList<String[]> movementEntries = dataSource.getAllMovementEntriesViaDate(pickedDate);
-        ArrayList<String> activitiesEntriesMorning = new ArrayList<>(), activitiesEntriesMidday = new ArrayList<>(), activitiesEntriesEvening = new ArrayList<>();
+        if(contentIntent.equals("Übersicht")){
+            Point[] activityPoints = new Point[0];
+            Point[] scorePoints;
+            ArrayList<Entry> entries  = dataSource.getAllEntries(pickedDate);
+            Integer activitiesEntriesMorning = 0, activitiesEntriesMidday = 0, activitiesEntriesEvening =0;
+            if(entries.size()!=0) {
+                for (int i = 0; i < entries.size(); i++) {
+                    if (entries.get(i).getActivities() != null) {
+                        if (entries.get(i).getDaytime().equals(getResources().getString(R.string.morning))) {
+                            activitiesEntriesMorning++;
+                        }
+                        if (entries.get(i).getDaytime().equals(getResources().getString(R.string.midday))) {
+                            activitiesEntriesMidday++;
+                        }
+                        if (entries.get(i).getDaytime().equals(getResources().getString(R.string.evening))) {
+                            activitiesEntriesEvening++;
+                        }
+                    }
+                }
+                ArrayList<Point> temp = new ArrayList<>();
+                if (activitiesEntriesMorning > 0) {
+                    temp.add(new Point(1, activitiesEntriesMorning));
+                }
+                if (activitiesEntriesMidday > 0) {
+                    temp.add(new Point(2, activitiesEntriesMidday));
+                }
+                if (activitiesEntriesEvening > 0) {
+                    temp.add(new Point(3, activitiesEntriesEvening));
+                }
+                activityPoints = new Point[temp.size()];
+                for (int k = 0; k < temp.size(); k++) {
+                    activityPoints[k] = temp.get(k);
+                }
+            }
 
-        Integer sensivitiesCounterMorning = 0, sensivitiesCounterMidday = 0, sensivitiesCounterEvening = 0;
-        dataSource.close();
+            ArrayList<String[]> scoreEntries = dataSource.getMainSymptomScoresViaNameAndDate(getResources().getString(R.string.key_score), pickedDate);
+            scorePoints = getPoints(scoreEntries);
 
-        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
-        staticLabelsFormatter.setHorizontalLabels(new String[]{getResources().getString(R.string.morning), getResources().getString(R.string.midday), getResources().getString(R.string.evening)});
-        graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
-        graph.getGridLabelRenderer().setVerticalLabelsVisible(false);
+            if(scorePoints.length==0 && activityPoints.length==0){
+                TextView textView = (TextView)findViewById(R.id.noDataTextView);
+                textView.setVisibility(View.VISIBLE);
+                Graph graph = new Graph.Builder()
+                        .setWorldCoordinates(-0.4, 4, -1, 6)
+                        .setXLabels(xLabels)
+                        .setYTicks(new double[] {0, 1, 2, 3, 4, 5})
+                        .build();
+                GraphView graphView = findViewById(R.id.graph_view);
+                graphView.setGraph(graph);
+            }
 
-        if(entries.size()!=0){
-           for(int i = 0; i<entries.size(); i++){
-               if(entries.get(i).getSensitivies()!=null){
-                   String[] allSensitivities = entries.get(i).getSensitivies().split(",");
-                   if(entries.get(i).getDaytime().equals(getResources().getString(R.string.morning))){
-                       sensivitiesCounterMorning += allSensitivities.length;
-                   }
-                   if(entries.get(i).getDaytime().equals(getResources().getString(R.string.midday))){
-                       sensivitiesCounterMidday += allSensitivities.length;
-                   }
-                   if(entries.get(i).getDaytime().equals(getResources().getString(R.string.evening))){
-                       sensivitiesCounterEvening += allSensitivities.length;
-                   }
-              }
-               if(entries.get(i).getActivities()!=null){
-                   if(entries.get(i).getDaytime().equals(getResources().getString(R.string.morning))){
-                       activitiesEntriesMorning.add(entries.get(i).getActivities());
-                   }
-                   if(entries.get(i).getDaytime().equals(getResources().getString(R.string.midday))){
-                       activitiesEntriesMidday.add(entries.get(i).getActivities());
-                   }
-                   if(entries.get(i).getDaytime().equals(getResources().getString(R.string.evening))){
-                       activitiesEntriesEvening.add(entries.get(i).getActivities());
-                   }
-               }
-           }
+            if(scorePoints.length==0){
+                Graph graph = new Graph.Builder()
+                        .setWorldCoordinates(-0.4, 4, -1, 6)
+                        .addLineGraph(activityPoints, getResources().getColor(R.color.colorActivites))
+                        .setXLabels(xLabels)
+                        .setYTicks(new double[]{0, 1, 2, 3, 4, 5})
+                        .build();
+                GraphView graphView = findViewById(R.id.graph_view);
+                graphView.setGraph(graph);
+                TextView textView = findViewById(R.id.graph_view_label_symptom);
+                textView.setVisibility(View.GONE);
+                TextView textViewActivity = findViewById(R.id.graph_view_label_activity);
+                textViewActivity.setTextColor(getResources().getColor(R.color.colorActivites));
+                textViewActivity.setVisibility(View.VISIBLE);
+            }
+
+            if(activityPoints.length==0) {
+                Graph graph = new Graph.Builder()
+                        .setWorldCoordinates(-0.4, 4, -1, 6)
+                        .addLineGraph(scorePoints, getResources().getColor(R.color.colorPrimary))
+                        .setXLabels(xLabels)
+                        .setYTicks(new double[]{0, 1, 2, 3, 4, 5})
+                        .build();
+                GraphView graphView = findViewById(R.id.graph_view);
+                graphView.setGraph(graph);
+                TextView textView = findViewById(R.id.graph_view_label_symptom);
+                textView.setTextColor(getResources().getColor(R.color.colorPrimary));
+                textView.setText(contentIntent);
+            }
+
+            else{
+                Graph graph = new Graph.Builder()
+                        .setWorldCoordinates(-0.4, 4, -1, 6)
+                        .addLineGraph(scorePoints, getResources().getColor(R.color.colorPrimary))
+                        .addLineGraph(activityPoints, getResources().getColor(R.color.colorActivites))
+                        .setXLabels(xLabels)
+                        .setYTicks(new double[]{0, 1, 2, 3, 4, 5})
+                        .build();
+                GraphView graphView = findViewById(R.id.graph_view);
+                graphView.setGraph(graph);
+                TextView textView = findViewById(R.id.graph_view_label_symptom);
+                textView.setTextColor(getResources().getColor(R.color.colorPrimary));
+                textView.setText(contentIntent);
+                TextView textViewActivity = findViewById(R.id.graph_view_label_activity);
+                textViewActivity.setTextColor(getResources().getColor(R.color.colorActivites));
+                textViewActivity.setVisibility(View.VISIBLE);
+            }
         }
-
         else{
-            TextView noDataTextView = (TextView)findViewById(R.id.noDataTextView);
-            noDataTextView.setVisibility(View.VISIBLE);
-            return;
+            Point[] pointString;
+            ArrayList<String[]> currentMainSymptomData = dataSource.getMainSymptomScoresViaNameAndDate(contentIntent, pickedDate);
+            if (currentMainSymptomData.size() != 0) {
+                pointString  = getPoints(currentMainSymptomData);
+
+                if (pointString.length > 0) {
+                    Graph graph = new Graph.Builder()
+                            .setWorldCoordinates(-0.4, 4, -1, 6)
+                            .addLineGraph(pointString, getResources().getColor(R.color.colorPrimary))
+                            .setXLabels(xLabels)
+                            .setYTicks(new double[]{0, 1, 2, 3, 4, 5})
+                            .build();
+                    GraphView graphView = findViewById(R.id.graph_view);
+                    graphView.setGraph(graph);
+                    TextView textView = findViewById(R.id.graph_view_label_symptom);
+                    textView.setTextColor(getResources().getColor(R.color.colorPrimary));
+                    textView.setText(contentIntent);
+                }
+            }
+
+            else{
+                TextView textView = (TextView)findViewById(R.id.noDataTextView);
+                textView.setVisibility(View.VISIBLE);
+                Graph graph = new Graph.Builder()
+                        .setWorldCoordinates(-0.4, 4, -1, 6)
+                        .setXLabels(xLabels)
+                        .setYTicks(new double[] {0, 1, 2, 3, 4, 5})
+                        .build();
+                GraphView graphView = findViewById(R.id.graph_view);
+                graphView.setGraph(graph);
+
+            }
         }
+    }
 
 
-      //  DataPoint[] dataPoints = new DataPoint[3];
+    private Point[] getPoints(ArrayList<String[]> currentMainSymptomData){
+        Point[] pointString;
+        if(currentMainSymptomData.size()>0) {
+            float counterMorning = 0, counterMidday = 0, counterEvening = 0, sumMorning = 0, sumMidday = 0, sumEvening = 0;
+            for (int j = 0; j < currentMainSymptomData.size(); j++) {
+                String currentDayTime = dateTimePicker.getDaytime(currentMainSymptomData.get(j)[2]);
+                if (currentDayTime.equals(getResources().getString(R.string.morning))) {
+                    counterMorning++;
+                    sumMorning += Integer.parseInt(currentMainSymptomData.get(j)[1]);
+                }
+                if (currentDayTime.equals(getResources().getString(R.string.midday))) {
+                    counterMidday++;
+                    sumMidday += Integer.parseInt(currentMainSymptomData.get(j)[1]);
+                }
+                if (currentDayTime.equals(getResources().getString(R.string.evening))) {
+                    counterEvening++;
+                    sumEvening += Integer.parseInt(currentMainSymptomData.get(j)[1]);
+                }
+            }
 
 
+            ArrayList<Point> temp = new ArrayList<>();
+            if (counterMorning > 0) {
+                temp.add(new Point(1, calculateAverage(sumMorning, counterMorning)));
+            }
+            if (counterMidday > 0) {
+                temp.add(new Point(2, calculateAverage(sumMidday, counterMidday)));
+            }
+            if (counterEvening > 0) {
+                temp.add(new Point(3, calculateAverage(sumEvening, counterEvening)));
+            }
+            pointString = new Point[temp.size()];
+            for (int k = 0; k < temp.size(); k++) {
+                pointString[k] = temp.get(k);
+            }
 
-        LineGraphSeries<DataPoint> seriesSensivities = new LineGraphSeries<>(new DataPoint[]{
-               new DataPoint(0, sensivitiesCounterMorning),
-               new DataPoint(1, sensivitiesCounterMidday),
-               new DataPoint(2, sensivitiesCounterEvening)
-        });
-        seriesSensivities.setTitle(getResources().getString(R.string.title_sensitivity));
-        seriesSensivities.setColor(getResources().getColor(R.color.colorText));
-        graph.addSeries(seriesSensivities);
-        graph.getLegendRenderer().setVisible(true);
-        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.BOTTOM);
-        graph.getLegendRenderer().setBackgroundColor(getResources().getColor(R.color.backgroundColorTheme));
-        graph.getLegendRenderer().setTextColor(getResources().getColor(R.color.colorText));
-
-
-        LineGraphSeries<DataPoint> seriesActivities = new LineGraphSeries<>(new DataPoint[] {
-               new DataPoint(0, activitiesEntriesMorning.size()),
-               new DataPoint(1, activitiesEntriesMidday.size()),
-               new DataPoint(2, activitiesEntriesEvening.size())
-        });
-        seriesActivities.setTitle(getResources().getString(R.string.title_movement));
-        seriesActivities.setColor(getResources().getColor(R.color.colorPrimary));
-        graph.addSeries(seriesActivities);
+        return pointString;
+        }
+        pointString = new Point[0];
+        return pointString;
     }
 
 
 
+    private int calculateAverage(float sum, float divisor){
+        return Math.round(sum / divisor);
+    }
 
-    //TODO: AUSLAGERN?
+
+
     private void initializeClickListenerForDatePickerButton(){
         datePickerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -167,12 +276,33 @@ public class GraphActivityDaily extends AppCompatActivity {
 
 
     private void initializeSpinner(){
+        String[] allSpinnerEntries = new String[mainSymptoms.length+1];
+        allSpinnerEntries[0] = getResources().getString(R.string.spinner_overview);
+        for (int i = 0; i < mainSymptoms.length; i++) {
+            allSpinnerEntries[i+1] = mainSymptoms[i];
+        }
         ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, R.layout.listentry_spinner, spinnerList);
-        //spinnerAdapter.setDropDownViewResource(R.layout.listentry_simple_listview);
         spinnerView.setAdapter(spinnerAdapter);
-        ArrayAdapter<String> spinnerGraphAdapter = new ArrayAdapter<String>(this, R.layout.listentry_spinner, spinnerListGraph);
+        ArrayAdapter<String> spinnerGraphAdapter = new ArrayAdapter<String>(this, R.layout.listentry_spinner, allSpinnerEntries);
         spinnerGraph.setAdapter(spinnerGraphAdapter);
+        spinnerGraph.setOnItemSelectedListener(this);
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if(++check > 1) {
+            Intent i = new Intent(getApplicationContext(), GraphActivityDaily.class);
+            i.putExtra(getResources().getString(R.string.key_spinner_graph), parent.getItemAtPosition(position).toString());
+            this.finish();
+            startActivity(i);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+    }
+
+
 
     @Override
     public void onBackPressed(){
